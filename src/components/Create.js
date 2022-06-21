1e305cb270d6b25648a526c2325c0df6db536c9b
Alejandro <vaquero.alejandro19@gmail.com>
2022-06-21 20:20:57 +0200

    starting the first project

---
+++ src/components/Create.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+function Create () {
+  const titleComponent= 'Añadir Película';
+
+  const [film, setFilm] = useState({
+    title: '',
+    description: ''
+  });
+
+  const {title, description} = film;
+
+  const getDataForm = (e) => {
+    e.preventDefault();
+
+    //Get Data from Form
+    let target = e.target;
+    let title = target.title.value;
+    let description = target.description.value;
+
+    //Crete an object of film we are going to add
+    let filmData = {
+      id: new Date().getTime(),
+      title: title,
+      description: description
+    };
+
+    setFilm(filmData);
+  }
+
+  return (
+    <div className="add">
+      <h3 className="title">{titleComponent}</h3>
+      
+      <strong>{(title && description) && "Has creado la peli: " + title}</strong>
+
+      <form onSubmit={getDataForm}>
+        <input type="text" name="title" placeholder="Titulo..." />
+        <textarea name="description" cols="30" rows="10" placeholder="Description..."></textarea>
+        <input type="submit" value="Guardar"/>
+      </form>
+    </div>
+  )
+}
+
+export default Create
